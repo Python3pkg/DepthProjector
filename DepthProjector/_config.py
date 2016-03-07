c4d975fabad3d72e5565db97f995a00aef9615a9
Ren Kanai <turutekab@gmail.com>
2016-03-07 12:14:07 +0900

    fix root path in _config.py

--- DepthProjector/_config.py
+++ DepthProjector/_config.py
@@ -2,9 +2,7 @@
 
 import os
 
-PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
-
-PATH_PSB_OBJ = os.path.join(PATH, 'psb_obj')
+PATH = os.path.curdir
 
 PATH_DEPTH_ARRAY = os.path.join(PATH, 'depth_arrays')
 PATH_DEPTH_IMG = os.path.join(PATH, 'depth_imgs')

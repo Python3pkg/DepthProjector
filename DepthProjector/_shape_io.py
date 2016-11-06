6941d4d49c208089babeec454aab3f40ea42eda0
kanairen <turutekab@gmail.com>
2016-11-07 00:46:06 +0900

    replace read_obj() in _shape_io.py.

--- DepthProjector/_shape_io.py
+++ DepthProjector/_shape_io.py
@@ -7,45 +7,33 @@ __author__ = 'kanairen'
 import numpy as np
 
 
-def read_obj(file_path):
+def read_obj(obj_file_path):
     """
-    objファイルを読み込み、頂点情報、法線情報、面情報を取得
-    :param file_path: ファイルパス
-    :return: 頂点リスト、法線リスト、面リスト
-    """
-    # 10.41s/10000time(v=298,f=492)
-    # vertices = []
-    # normals = []
-    # faces = []
-    # with open(file_path) as f:
-    #     while True:
-    #         line = f.readline()
-    #         if line == '':
-    #             break
-    #         items = line.strip().split()
-    #         if len(items) == 0:
-    #             continue
-    #         if items[0] == 'v':
-    #             vertices.append(list(map(float, items[1:])))
-    #         elif items[0] == 'vn':
-    #             normals.append(list(map(float, items[1:])))
-    #         elif items[0] == 'f':
-    #             faces.append(list(map(int, items[1:])))
 
-    # 8.98s/10000time(v=298,f=492)
-    with open(file_path) as f:
-        lines = filter(lambda x: len(x) > 0,
+    .objファイルを読み込み
+
+    :type obj_file_path : str
+    :param obj_file_path: ファイルパス
+
+    :rtype: [np.ndarray, np.ndarray, np.ndarray]
+    :return: 頂点配列、法線構成頂点インデックス配列、面構成頂点インデックス配列
+
+    """
+
+    with open(obj_file_path) as f:
+        lines = filter(lambda x: x != "\n" and x[0] != "#",
                        [line.strip().split() for line in f.readlines()])
-        vertices = [list(map(float, line[1:])) for line in lines if
-                    line[0] == 'v']
-        normals = [list(map(float, line[1:])) for line in lines if
-                   line[0] == 'vn']
 
-        def split_face_info(s):
-            return [int(s) if s.isdigit() else None for s in s.split('/')]
-
-        faces = [list(map(split_face_info, line[1:])) for line in lines if
-                 line[0] == 'f']
+        vertices = np.array(
+            [list(map(float, line[1:])) for line in lines if
+             line[0] == 'v'])
+        normals = np.array(
+            [list(map(float, line[1:])) for line in lines if
+             line[0] == 'vn'])
+        faces = np.array(
+            [list(map(lambda x: x - 1,
+                      map(int, map(lambda x: x.split('/')[0], line[1:])))) for
+             line in lines if line[0] == 'f'])
 
     return Obj(vertices, normals, faces)
 
